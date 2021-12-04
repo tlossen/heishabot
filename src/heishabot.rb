e93386dbb6bcc38f31131dc0080327ea44823c02
Tim Lossen <tim@lossen.de>
2021-12-04 19:53:40 +0100

    better log output

--- src/heishabot.rb
+++ src/heishabot.rb
@@ -1,13 +1,16 @@
 require 'rubygems'
 require 'mqtt'
 
+MOSQUITTO_HOST = "localhost"
 INFLUX_URL = "http://localhost:8086/write?db=heishamon"
 
 # connect to local mosquitto instance
-MQTT::Client.connect('localhost') do |c|
+MQTT::Client.connect(MOSQUITTO_HOST) do |c|
+  puts "#{Time.now} connected to mosquitto on #{MOSQUITTO_HOST}"
+
   # subscribe to heishamon topics
   c.get("panasonic_heat_pump/#") do |topic, message|
-    puts "#{topic}: #{message}"
-    system "curl -XPOST '#{INFLUX_URL}' --data-binary '#{key} value=#{message}'"
+    puts "#{Time.now} #{topic}: #{message}"
+    system "curl -XPOST '#{INFLUX_URL}' --data-binary '#{topic} value=#{message}'"
   end
 end
