522bde5899e166e0e370109b817b8fd11acf9aa6
Tim Lossen <tim@lossen.de>
2021-12-04 19:26:35 +0100

    write to influxdb

--- src/heishabot.rb
+++ src/heishabot.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
 require 'mqtt'
 
+INFLUX_URL = "http://localhost:8086/write?db=heishamon"
+
 # connect to local mosquitto instance
 MQTT::Client.connect('localhost') do |c|
   # subscribe to heishamon topics
-  c.get("panasonic_heat_pump") do |topic, message|
+  c.get("panasonic_heat_pump/#") do |topic, message|
     puts "#{topic}: #{message}"
+    system "curl -XPOST '#{INFLUX_URL}' --data-binary '#{key} value=#{message}'"
   end
 end
