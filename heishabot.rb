c7e7a52b8a6079adcf8540b7333704566e2aaac8
Tim Lossen <tim@lossen.de>
2021-12-06 22:06:01 +0100

    remove crystal version

--- heishabot.rb
+++ heishabot.rb
@@ -5,12 +5,13 @@ MOSQUITTO_HOST = "localhost"
 INFLUX_URL = "http://localhost:8086/write?db=heishamon"
 
 # connect to local mosquitto instance
-MQTT::Client.connect(MOSQUITTO_HOST) do |c|
+MQTT::Client.connect(MOSQUITTO_HOST) do |mqtt|
   puts "#{Time.now} connected to mosquitto on #{MOSQUITTO_HOST}"
 
-  # subscribe to heishamon topics
-  c.get("panasonic_heat_pump/#") do |topic, message|
+  # subscribe to all heishamon topics
+  mqtt.get("panasonic_heat_pump/#") do |topic, message|
     puts "#{Time.now} #{topic}: #{message}"
+    # store each message in influxdb
     system "curl -XPOST '#{INFLUX_URL}' --data-binary '#{topic} value=#{message}'"
   end
 end
