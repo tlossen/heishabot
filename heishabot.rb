16d5d8b5be58f9a98fa76efea4f5d4179bb5c15e
Tim Lossen <tim@lossen.de>
2021-12-08 11:22:57 +0100

    reconnect to mosquitto, offload curl calls to worker queue

--- heishabot.rb
+++ heishabot.rb
@@ -1,21 +1,32 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'mqtt'
+require 'work_queue'
 
 MOSQUITTO_HOST = "localhost"
 INFLUX_URL = "http://localhost:8086/write?db=heishamon"
 
-# wait for startup of mosquitto & influxdb
-#sleep(45)
+queue = WorkQueue.new(5, nil)
 
-# connect to local mosquitto instance
-MQTT::Client.connect(MOSQUITTO_HOST) do |mqtt|
-  puts "#{Time.now} connected to mosquitto on #{MOSQUITTO_HOST}"
+# keep reconnecting
+while true do
+  begin
+  # connect to local mosquitto instance
+    MQTT::Client.connect(MOSQUITTO_HOST) do |mqtt|
+      puts "#{Time.now} connected to mosquitto on #{MOSQUITTO_HOST}"
 
-  # subscribe to all heishamon topics
-  mqtt.get("panasonic_heat_pump/#") do |topic, message|
-    puts "#{Time.now} #{topic}: #{message}"
-    # store each message in influxdb
-    system "curl -XPOST '#{INFLUX_URL}' --data-binary '#{topic} value=#{message}'"
+      # subscribe to all heishamon topics
+      mqtt.get("panasonic_heat_pump/#") do |topic, message|
+
+        # store each message in influxdb
+        queue.enqueue_b do 
+          puts "#{Time.now} #{topic}: #{message}"
+          system "curl -XPOST '#{INFLUX_URL}' --data-binary '#{topic} value=#{message}'"
+        end
+      end
+    end
+  rescue Errno::ECONNREFUSED
+    puts "connecting to mosquitto on #{MOSQUITTO_HOST} ..."
+    sleep(5)
   end
 end
